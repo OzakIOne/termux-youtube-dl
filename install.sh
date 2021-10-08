1b709749b654bf652e006bd27265ccbd398dccc6
ozakione <29860391+OzakIOne@users.noreply.github.com>
2021-10-08 22:56:25 +0200

    feat: change to yt-dlp

--- install.sh
+++ install.sh
@@ -20,13 +20,13 @@ termux-setup-storage
 sleep 2
 apt-get update
 apt-get -y install python ffmpeg
-pip install youtube-dl
+pip install yt-dlp
 mkdir -p $YOUTUBEDL_OUTPUT_FOLDER
 mkdir -p $YOUTUBEDL_CONFIG_FOLDER
 mkdir -p $TERMUXURLOPENER_CONFIG_FOLDER
 # Download setup & config file
-curl -L https://raw.githubusercontent.com/OzakIOne/termux-youtube-dl/v1.0.1/.ytdl.conf > "${YOUTUBEDL_CONFIG_FOLDER}/config"
-curl -L https://raw.githubusercontent.com/OzakIOne/termux-youtube-dl/v1.0.1/share2youtubedl.sh > "${TERMUXURLOPENER_CONFIG_FOLDER}/termux-url-opener"
+curl -L https://raw.githubusercontent.com/OzakIOne/termux-youtube-dl/v1.0.2/.ytdl.conf > "${YOUTUBEDL_CONFIG_FOLDER}/config"
+curl -L https://raw.githubusercontent.com/OzakIOne/termux-youtube-dl/v1.0.2/share2youtubedl.sh > "${TERMUXURLOPENER_CONFIG_FOLDER}/termux-url-opener"
 
 echo -e "${BLUE}Congratulations!!! Your setup is complete.\n\n"
 read -p "When you are ready just press enter"
--- share2youtubedl.sh
+++ share2youtubedl.sh
@@ -2,20 +2,20 @@
 clear
 
 if [[ "$1" =~ ^.*youtu.*$ ]] || [[ "$1" =~ ^.*youtube.*$ ]]; then
-  echo -e "Downloading video...\\n>URL: ${1}\\n"
-  youtube-dl -F "$1"
-  read -p "Choose your video quality (press enter for: 'best') : " video
-  read -p "Choose your audio quality (press enter for: 'best') : " audio
-  if [[ "$video" = "" ]]; then
-    video="best"
-  fi
-  if [[ "$audio" = "" ]]; then
-    audio="best"
-  fi
-  youtube-dl -f "$video"+"$audio" "$1"
-elif [[ "$1" =~ ^.*nourlselected.*$ ]]; then
-  echo "There was an error"
-else youtube-dl -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best' "$1"
+    echo -e "Downloading video...\\n>URL: ${1}\\n"
+    yt-dlp -F "$1"
+    read -p "Choose your video quality (press enter for: 'best') : " video
+    read -p "Choose your audio quality (press enter for: 'best') : " audio
+    if [[ "$video" = "" ]]; then
+        video="best"
+    fi
+    if [[ "$audio" = "" ]]; then
+        audio="best"
+    fi
+    yt-dlp -f "$video"+"$audio" "$1"
+    elif [[ "$1" =~ ^.*nourlselected.*$ ]]; then
+    echo "There was an error"
+else yt-dlp -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best' "$1"
 fi
 
 read -p "Press enter to continue"
