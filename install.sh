f34f8f3681d07ad5cb901d2037307e899a8e5da5
ozaki <29860391+OzakIOne@users.noreply.github.com>
2022-12-21 12:03:02 +0100

    fix: config file not found

--- install.sh
+++ install.sh
@@ -25,6 +25,7 @@ mkdir -p $YOUTUBEDL_CONFIG_FOLDER
 mkdir -p $TERMUXURLOPENER_CONFIG_FOLDER
 # Download setup & config file
 curl -L https://raw.githubusercontent.com/OzakIOne/termux-youtube-dl/v2.0.0/.ytdl.conf >"${YOUTUBEDL_CONFIG_FOLDER}/config"
+curl -L https://raw.githubusercontent.com/OzakIOne/termux-youtube-dl/v2.0.0/sponsorblock.conf >"${YOUTUBEDL_CONFIG_FOLDER}/sponsorblock.conf"
 curl -L https://raw.githubusercontent.com/OzakIOne/termux-youtube-dl/v2.0.0/share2youtubedl.sh >"${TERMUXURLOPENER_CONFIG_FOLDER}/termux-url-opener"
 
 echo -e "${BLUE}Congratulations!!! Your setup is complete.\n\n"
